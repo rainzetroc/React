ecaa1044208d8e357ffd8e72ccc9e0f1fd68af85
Laarni Cortez <RainZetroc@hotmail.com>
2021-06-03 11:13:11 +0800

    changes

---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,29 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-af5f0-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
